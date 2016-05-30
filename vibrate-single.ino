54312f1178f2adeb34e6e6b5237aad8b0cb935f0
Peter Juras <p.juras@outlook.com>
2016-05-30 16:18:56 +0200

    May 30 sketch

---
+++ vibrate-single.ino
@@ -0,0 +1,28 @@
+const int engine1 = 6;
+const int engine2 = 10;
+const int engine3 = 11;
+
+void setup() {
+  pinMode(engine1, OUTPUT);
+  pinMode(engine2, OUTPUT);
+  pinMode(engine3, OUTPUT);
+}
+
+void vibrate(int engine, int strength, int ms) {
+  analogWrite(engine, strength);
+  delay(ms);
+}
+
+void vibrateSingle(int engine, int strength, int ms) {
+  analogWrite(engine1, 0);
+  analogWrite(engine2, 0);
+  analogWrite(engine3, 0);
+
+  vibrate(engine, strength, ms);
+}
+
+void loop() {
+  vibrateSingle(engine1, 255, 50);
+  vibrateSingle(engine2, 255, 50);
+  vibrateSingle(engine3, 255, 50);
+}
