dae98849a2a7d97ccffca53a01102cd4917499f5
Peter Juras <p.juras@outlook.com>
2016-07-01 16:08:11 +0200

    Led pixel update

--- led-pixel/led-pixel.ino
+++ led-pixel/led-pixel.ino
@@ -1,37 +1,85 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 3
+#define PIN2 5
+
 const int pinX = A0;
 const int pinY = A1;
 const int pinZ = A2;
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+const int pinButton = 2;
 
+Adafruit_NeoPixel stickStrip = Adafruit_NeoPixel(29, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel dockStrip = Adafruit_NeoPixel(30, PIN2, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  // State button
+  pinMode(pinButton, INPUT);
+  
+  stickStrip.begin();
+  stickStrip.show(); // Initialize all pixels to 'off'
+
+  dockStrip.begin();
+  dockStrip.show(); // Initialize all pixels to 'off'
+  
+  stickStrip.setBrightness(15);
+  dockStrip.setBrightness(255);
+  
   Serial.begin(9600);
 }
 
+
+int buttonState = 0;
+int lastButtonState = 0;
+int pushes = 0;
+
 void loop() {
-  strip.setBrightness(50);
-  colorAllLeds(redOrGreen(analogRead(pinZ)));
+  Serial.println(digitalRead(pinButton));
+  buttonState = digitalRead(pinButton);
+  if (buttonState != lastButtonState && buttonState == HIGH) {
+      pushes++;
+      delay(50);
+  }
+  lastButtonState = buttonState;
+  
+  if (pushes % 2 == 0) {
+    ambientLight();
+  } else {
+    decisionMaking();
+  }
 }
 
-uint32_t redOrGreen(int input) {
-  float relativeToNormal = 350 - input;
+void ambientLight() {
+  // 239 235 208
+//  colorAllLeds(dockStrip, dockStrip.Color(255, 200, 50));
+//  colorAllLeds(stickStrip, stickStrip.Color(255, 200, 50));
+  colorAllLeds(dockStrip, dockStrip.Color(255, 0, 255));
+  colorAllLeds(stickStrip, stickStrip.Color(255, 0, 255));
+}
+
+void decisionMaking() {
+  int sensorInput = analogRead(pinZ);
+  uint32_t color1 = redOrGreen(stickStrip, sensorInput);
+  uint32_t color2 = redOrGreen(dockStrip, sensorInput);
+  colorAllLeds(stickStrip, color1);
+  colorAllLeds(dockStrip, color2);
+}
+
+uint32_t redOrGreen(Adafruit_NeoPixel & strip, int input) {
+  float relativeToNormal = 380 - input;
   if (relativeToNormal < 0) {
     relativeToNormal = 0;
   }
 
-  float scale = (relativeToNormal / 70) * 255;
+  float scale = (relativeToNormal / 100) * 255;
+  if (scale > 255) {
+    scale = 255;
+  }
   return strip.Color(255 - scale, scale, 0);
 }
 
-void colorAllLeds(uint32_t color) {
+void colorAllLeds(Adafruit_NeoPixel & strip, uint32_t color) {
   for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, color);
-    strip.show();
   }
+  strip.show();
 }
